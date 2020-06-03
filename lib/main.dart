b08ffee0341b455371141a841a2a7c1273bc4fc0
Infinite-eyes <cwm930215@gmail.com>
2020-06-03 16:57:47 +0800

    1.add setState

--- lib/main.dart
+++ lib/main.dart
@@ -31,18 +31,19 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-
   final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Startup Name Cenerator'),
-        ),
-        body: _buildSuggestions(),
-      );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Cenerator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 
   @override
@@ -50,24 +51,39 @@ class RandomWordsState extends State<RandomWords> {
     return new ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
-          if(i.isOdd) return new Divider();
+          if (i.isOdd) return new Divider();
 
-          final index = i~/2;
-          if(index >= _suggestions.length){
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
-    });
+        });
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
     return new ListTile(
-      title:  new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
+        title: new Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: new Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        }
+
     );
   }
-
-
 }
