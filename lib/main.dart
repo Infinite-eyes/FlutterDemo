47a38658e14e5fb30556c44db79f8c1650a92a2c
Infinite-eyes <cwm930215@gmail.com>
2020-06-04 10:49:35 +0800

    1.Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,58 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: 'My app',
-    home: new MyScaffold(),
+    title: 'Flutter Tutorial',
+    home: new TutorialHome(),
   ));
 }
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-
-  final Widget title;
-
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: new BoxDecoration(color: Colors.blue[500]),
-      child: new Row(
-        children: <Widget>[
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(
+            icon: new Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null),
+        title: new Text('Example title'),
+        actions: <Widget>[
           new IconButton(
-              icon: Icon(Icons.menu),
-              tooltip: 'Navigation menu',
-              onPressed: null),
-          new Expanded(
-            child: title,
-          ),
-          new IconButton(
-              icon: new Icon(Icons.search), tooltip: 'Search', onPressed: null)
+              icon: new Icon(Icons.search), tooltip: 'Search', onPressed: null),
         ],
       ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Material(
-      child: new Column(
-        children: <Widget>[
-          new MyAppBar(
-            title: new Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.title,
-            ),
-          ),
-          new Expanded(
-            child: new Center(
-              child: new Text('Hello, world!'),
-            ),
-          ),
-        ],
+      body: new Center(
+        child: new Text('Hello, world'),
       ),
+      floatingActionButton: new FloatingActionButton(
+          tooltip: 'Add', child: new Icon(Icons.add), onPressed: null),
     );
   }
 }
