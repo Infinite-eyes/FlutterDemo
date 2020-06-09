b6bfef68a9ce6c5db8de6d627e8bed9e6a5aa795
Infinite-eyes <cwm930215@gmail.com>
2020-06-09 12:17:43 +0800

    1.change bloc

--- lib/page/counter_page_bloc.dart
+++ lib/page/counter_page_bloc.dart
@@ -8,6 +8,10 @@ class CounterBlocPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final IncrementBloc bloc = BlocProvider.of<IncrementBloc>(context);
 
+
+    //这样也可以但是数据暴露在外层了
+    var _counter = 1;
+
     return Scaffold(
       appBar: AppBar(title: Text('Stream version of the Counter App')),
       body: Center(
@@ -22,7 +26,7 @@ class CounterBlocPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          bloc.incrementCounter.add(null);
+          bloc.incrementCounter.add(_counter++);
         },
       ),
     );
@@ -33,31 +37,33 @@ class IncrementBloc implements BlocBase {
 
   int _counter;
 
-  StreamController<int> _counterController = StreamController<int>();
-//
-  StreamSink<int> get _inAdd => _counterController.sink;
-//
-  Stream<int> get outCounter => _counterController.stream;
+//  StreamController<int> _counterController = StreamController<int>();
+////
+//  StreamSink<int> get _inAdd => _counterController.sink;
+////
+//  Stream<int> get outCounter => _counterController.stream;
 
-  StreamController _actionController = StreamController();
+  StreamController<int> _actionController = StreamController();
+
+  Stream<int> get outCounter => _actionController.stream;
 
   StreamSink get incrementCounter => _actionController.sink;
 
   IncrementBloc() {
     _counter = 0;
-    _actionController.stream.listen(_handleLogic);
+//    _actionController.stream.listen(_handleLogic);
   }
 
   @override
   void dispose() {
     _actionController.close();
-    _counterController.close();
+//    _counterController.close();
   }
 
-  void _handleLogic(data) {
-    _counter = _counter + 1;
-    _inAdd.add(_counter);
-  }
+//  void _handleLogic(data) {
+//    _counter = _counter + 1;
+//    _inAdd.add(_counter);
+//  }
 }
 
 abstract class BlocBase {
