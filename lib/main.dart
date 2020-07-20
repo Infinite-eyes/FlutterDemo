e0c2f22f0b3c98561d2e5b5b68231e5199881cda
Infinite-eyes <cwm930215@gmail.com>
2020-07-20 23:41:34 +0800

    1.add base structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //new 关键词可以省略 v1
-//  runApp(new Center(
-//      child: new Text(
-//    '你好flutter',
-//    textDirection: TextDirection.ltr,
-//  )));
-//v2
-//  runApp(Center(
-//      child: Text(
-//    '你好flutter',
-//    textDirection: TextDirection.ltr,
-//  )));
-
-// v3
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HomeContent();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("appbar title holder"),
+        ),
+        body: HomeContent(),
+      ),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
   }
 }
 
