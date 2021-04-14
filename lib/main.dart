b07b8a2291ca9bef5bb0e3ff69335747f88d4b7a
Infinite-eyes <cwm930215@gmail.com>
2021-04-14 18:02:24 +0800

    1.add  drawing demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,64 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:flutter/rendering.dart';
 
+// https://xieguanglei.github.io/blog/post/flutter-code-chapter-01.html
 void main() {
-  runApp(MyApp());
-}
+  //v1 draw line
+  // PictureRecorder recorder = PictureRecorder();
+  // Canvas canvas = Canvas(recorder);
+  //
+  // Paint p = Paint();
+  // p.strokeWidth = 30.0;
+  // p.color = Color(0xFFFF00FF);
+  //
+  // canvas.drawLine(Offset(300, 300), Offset(800, 800), p);
+  // Picture picture = recorder.endRecording();
+  //
+  // SceneBuilder sceneBuilder = SceneBuilder();
+  // sceneBuilder.pushOffset(0, 0);
+  // sceneBuilder.addPicture(new Offset(0, 0), picture);
+  // sceneBuilder.pop();
+  // Scene scene = sceneBuilder.build();
+  //
+  // window.onDrawFrame = () {
+  //   window.render(scene);
+  // };
+  // window.scheduleFrame();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  //v2
+  PictureRecorder recorder = PictureRecorder();
+  Canvas canvas = Canvas(recorder);
+  Paint p = Paint();
+  p.strokeWidth = 30.0;
+  p.color = Color(0xFF0099FF);
+  canvas.drawLine(Offset(300, 300), Offset(800, 800), p);
+  canvas.drawLine(Offset(800, 300), Offset(300, 800), p);
+  Picture picCross = recorder.endRecording();
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  window.onDrawFrame = () {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    int i = DateTime.now().millisecond;
+    PictureRecorder recorder = PictureRecorder();
+    Canvas canvas = Canvas(recorder);
+    canvas.drawLine(Offset(i * 0.2, 550), Offset(1080 - i * 0.2, 550), p);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    Picture picLine = recorder.endRecording();
 
-  final String title;
+    SceneBuilder sceneBuilder = SceneBuilder();
+    sceneBuilder.pushOffset(0, 0);
+    sceneBuilder.pushOpacity(128);
+    sceneBuilder.addPicture(new Offset(0, 0), picCross);
+    sceneBuilder.pop();
+    sceneBuilder.pushOffset(0, 0.5 * (i - 500));
+    sceneBuilder.addPicture(new Offset(0, 0), picLine);
+    sceneBuilder.pop();
+    sceneBuilder.pop();
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+    Scene scene = sceneBuilder.build();
+    window.render(scene);
+    scene.dispose();
+    window.scheduleFrame();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have clicked3 the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  };
+  window.scheduleFrame();
 }
